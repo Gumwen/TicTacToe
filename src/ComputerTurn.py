502900696794eb605e69c932194eb811b970e293
Osamwengumwenro Oni-Ojo <146798042+Gumwen@users.noreply.github.com>
2024-12-05 02:38:02 -0600

    Update ComputerTurn.py to choose O if human chooses X and vice versa

--- src/ComputerTurn.py
+++ src/ComputerTurn.py
@@ -1,8 +1,9 @@
 import random
 
-def computer_turn(board):
+def computer_turn(board, symbol):
+    opp_symbol = "X" if symbol == "O" else "O"
     while True:
         row, col = random.randint(0, 2), random.randint(0, 2)
         if board[row][col] == " ":
-            board[row][col] = "O"
+            board[row][col] = symbol
             break
