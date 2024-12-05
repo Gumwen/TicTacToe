c75e71271c88f98bbce425fe5eb672fe88acc0a4
Osamwengumwenro Oni-Ojo <146798042+Gumwen@users.noreply.github.com>
2024-12-05 03:44:38 -0600

    Updated WinOrLose.py, made code more interactive, fixed errors

--- src/WinOrLose.py
+++ src/WinOrLose.py
@@ -1,16 +1,28 @@
-def check_win_or_draw(board):
-    # Check rows and columns
+def check_win_or_draw(board, human_symbol):
+    # Check the rows and columns
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2] != " ":
-            return f"{board[i][0]} wins!"
+            if board[i][0] == human_symbol:
+                return "Player wins!" 
+            else:
+                return "Computer wins!"  
         if board[0][i] == board[1][i] == board[2][i] != " ":
-            return f"{board[0][i]} wins!"
+            if board[0][i] == human_symbol:
+                return "Player wins!"
+            else:
+                return "Computer wins!"
 
-    # Check diagonals
+    # Check the diagonals
     if board[0][0] == board[1][1] == board[2][2] != " ":
-        return f"{board[0][0]} wins!"
+        if board[0][0] == human_symbol:
+            return "Player wins!"
+        else:
+            return "Computer wins!"
     if board[0][2] == board[1][1] == board[2][0] != " ":
-        return f"{board[0][2]} wins!"
+        if board[0][2] == human_symbol:
+            return "Player wins!"
+        else:
+            return "Computer wins!"
 
     # Check for draw
     if all(board[row][col] != " " for row in range(3) for col in range(3)):
