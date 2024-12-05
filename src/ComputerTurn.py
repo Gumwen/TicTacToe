94e66495158f6a4d0f684dad682c97935e5b4b60
Osamwengumwenro Oni-Ojo <146798042+Gumwen@users.noreply.github.com>
2024-12-05 02:53:31 -0600

    Update ComputerTurn.py, removed unnecessary line of code

--- src/ComputerTurn.py
+++ src/ComputerTurn.py
@@ -1,7 +1,6 @@
 import random
 
 def computer_turn(board, symbol):
-    opp_symbol = "X" if symbol == "O" else "O"
     while True:
         row, col = random.randint(0, 2), random.randint(0, 2)
         if board[row][col] == " ":
